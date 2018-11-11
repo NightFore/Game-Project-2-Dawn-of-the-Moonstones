1407b0f3b6beab515738e6074cc96a9b2317eaf4
NightFore <38656758+NightFore@users.noreply.github.com>
2018-11-11 20:31:12 +0100

    [Python] Game Project 2 - Dawn of the Moonstones

---
+++ [Master Build]/[Setup] Dawn on the Moonstones v6-0b/setup.py
@@ -0,0 +1,17 @@
+import cx_Freeze
+import os.path
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
+executables = [cx_Freeze.Executable("Dawn of the Moonstones v6-0b.py")]
+
+cx_Freeze.setup(
+    name="Dawn of the Moonstones",
+    options={"build_exe": {"packages": ["pygame"],
+                           "include_files": ["pygame_textinput.py",
+                                             "Data"
+                                             "readme.txt"]}},
+    executables = executables
+
+    )
